cbb763fbeda92025ef9b72391aed136c081e2d84
Stream5937 <149094480+Stream5937@users.noreply.github.com>
2024-02-20 08:27:19 +0000

    correct equals error - final commit

--- calc.js
+++ calc.js
@@ -13,6 +13,7 @@ let first = null;
 let second = null;
 let operator = null;
 let keysEntered = '';       //empty string to store seqence of keyed entries used to build values
+let delkeys = '';           //saved keysEntered if need to adjust
 let valuesEntered = [];     //empty array to store possible three entered values [first, operator, second]
 let numLength = 0;          //to enable check the entered num value no longer than content display length
 let decimal = false;        // no decimal yet
@@ -28,12 +29,13 @@ const resultObj = {
     }
 */
 
+/*
 //****** dev only ******
 const CLEARSTORAGE = false;
 //const CLEARSTORAGE = true;
 if(CLEARSTORAGE){localStorage.clear();console.log("storage cleared");}
 //****** dev only ******
-
+*/
 //functions
 //DISABLE SCROLL
 function noflow () {
@@ -86,10 +88,9 @@ function showHistory() {
             console.log(prev.textContent);
         }
   });
-  //updateHistories();
 }
 
-
+/* LOCAL STORAGE NOT NEEDED
 //action local storage of 'items' 
 //initialise storage for array of items
 function initialiseLocalStorage(items){
@@ -124,48 +125,22 @@ function removeLocal(items, item){
 function clearStorage() {
     localStorage.clear();
 }
-
+*/
 //validate floating point numeric input
 function validateInput(input){
     num = input;
     let test;
-    //console.log(`num is ${num}`);
-    //console.log(`->first: ${first}, second: ${second}.`);
-    //for test
-    //test=1;
-    //first=null; second = null;
-    //test=2;
-    //first = 10; second = null;
-    //test=3;
-    //first = 10; second = 20;
-    //console.log(`pretest: ${test}, first: ${first}, second: ${second}.`);
-    /*
-    if(!(second === null) && !(first === null)){
-        console.log("Display input error");
-        content.textContent='Input Error';
-    }
-    */
+ 
     if(second == null && first == null) {
         first = num;
         valuesEntered.push(first);
-        //console.log(`test: ${test}, first: ${first}, second: ${second}.`);
-        //content.textContent ='';
-        //content.textContent = valuesEntered[0].toString();
         setContent(valuesEntered[0].toString());
         //reset decimal flag
         decimal = false;
     }else{
-        // console.log(`at else`);
-        // console.log(`at-else-test: ${test}, first: ${first}, second: ${second}.`);
-        // let type = typeof first;
-        // console.log(`type of first ${type}`)
-        if((second === null) && !(first === null)) {
+               if((second === null) && !(first === null)) {
             second = num;
             valuesEntered.push(second);
-           // console.log(`-test: ${test}, first: ${first}, second: ${second}.`);
-           // console.log(`values: ${valuesEntered[0]}, ${valuesEntered[1]}, ${valuesEntered[2]}`);
-            //content.textContent ='';
-            //content.textContent = valuesEntered[0].toString()+' '+valuesEntered[1].toString()+' '+valuesEntered[2].toString();
             setContent(valuesEntered[0].toString()+' '+valuesEntered[1].toString()+' '+valuesEntered[2].toString());
             //reset decimal flag
             decimal = false;
@@ -214,22 +189,7 @@ function operate ( firstVal, operator, secondVal){
             return result;
         }
         case '/':{
-           // console.log('dividing');
-            /*
             if(secondVal === 0) {
-                content.textContent="Division by zero error!";
-                console.log('***division by zero***');
-            }
-            */
-            if(secondVal === 0) {
-                /*
-               //my delay
-               for(let i =0; i< 100; i++){
-                for(let j=0; j<100;j++){
-                    console.log("inner timer");
-                }
-               }
-               */
                return firstVal;
             }else{
                 return divide(firstVal,secondVal);
@@ -268,30 +228,19 @@ function btnClicked(id) {
         case '9': {
             num = parseInt(id);
             keysEntered += num;
-            //console.log(`236- keysEntered= ${keysEntered}`);
-            //console.log(`last was operator ${lastValueWasOperator}`);
            if(lastValueWasOperator){
-               // content.textContent= '';
-                //content.textContent= `${valuesEntered[0]} ${valuesEntered[1]} ${keysEntered}`;
                 setContent(`${valuesEntered[0]} ${valuesEntered[1]} ${keysEntered}`);
             }else{
-               // console.log(`236- valuesEntered[0] = ${valuesEntered[0]}`);
                 if(valuesEntered[0]== undefined){
-                    //console.log("238- at a");
-                   // content.textContent= '';
-                    //content.textContent= keysEntered;
                     setContent(keysEntered);
                 }else{
-                    //console.log("242-at b");
                     //remember key just entered
                     let temp = keysEntered;
                     //clear everything but history and restart
-                    //modifyInput('clr');
-                    console.log('clr all but except history' );
+                    //console.log('clr all but except history' );
                     content.textContent='';
                     keysEntered = '';
                     valuesEntered = [];
-                    //console.log(`342- ${valuesEntered[0]}`);
                     numLength = 0;
                     decimal = false;
                     lastValueWasOperator=false;
@@ -301,12 +250,9 @@ function btnClicked(id) {
                     result = 0;
                     //reinstate just entered key
                     keysEntered = temp;
-                   // content.textContent= '';
-                    //content.textContent= keysEntered; 
                     setContent(keysEntered); 
                 }
            }
-            //validateInput(num);
             break;
         }
 
@@ -324,25 +270,19 @@ function btnClicked(id) {
         case 'del':             //delete last input
         case 'sign':            //change input sign
         {
-           // console.log(`alter input id ${id}`);
             modifyInput(id);
             break;
         }
 
         //handle decimal point
         case'dp': {
-            //console.log(`decimal point ${id}`);
-            //check no dp already
-            //if(! keysEntered.includes('.')){
             if(!decimal){
                 //flag decimal true
                 decimal = true;
                 keysEntered = keysEntered+'.';
-                //console.log(`keysEntered`);
                 //log the length of keysEntered string including dp
                 //for later check value no more than content display width
                 numLength = keysEntered.length;
-                //content.textContent = keysEntered;
                 if(lastValueWasOperator){
                     //must be second value
                     setContent(`${valuesEntered[0].toString()} ${valuesEntered[1].toString()} ${keysEntered}`);
@@ -364,13 +304,15 @@ function btnClicked(id) {
                 //then just complete by reset of id -> equals  
                 id = '=';
             }else{
-                //console.log(`action operator ${id}`);
                 //operator indicates end of first input
                 if(first === null){
                     //so save first input number value
                     validateInput(parseFloat(keysEntered));
                 }
-                // valuesEntered.push(keysEntered);
+                //remember keysEntered for 'del' action
+                keysEntered = keysEntered+id;
+                delkeys = keysEntered.slice(0) ;     //a copy
+                console.log(`keysEntered -> ${keysEntered}, delKeys = ${delkeys}`);
                 //empty the keysEntered string ready for second number value
                 keysEntered = '';
                 //store the operator
@@ -388,25 +330,32 @@ function btnClicked(id) {
         }
 
         case '=': {
-            //store the last value
-            valuesEntered.push(parseFloat(keysEntered));
-            //reset decimal flag
-            decimal = false;
-            //display expression
-            //content.textContent ='';
-            //content.textContent = valuesEntered[0].toString()+' '+valuesEntered[1].toString()+' '+valuesEntered[2].toString();
-            //check for division by zero error
-            if((valuesEntered[1] === '/') && (valuesEntered[2] === 0 )){
-                setContent('Division by zero error!');
-                valuesEntered[2] = null;
-            }else{
-                validateInput(parseFloat(keysEntered));
-                obtainResult();
-                //cancel previous
-                lastValueWasOperator = false;
-                //console.log('#.. = ${result}');
-                //content.textContent=result;
+            console.log(lastValueWasOperator);
+            //have we had an operator 
+            if(lastValueWasOperator){
+                console.log("at337");
+                //do we have a value
+                if(!(keysEntered === '')){
+                    console.log("at340");
+                    console.log(keysEntered);
+                    //store the last value
+                    valuesEntered.push(parseFloat(keysEntered));
+                    //reset decimal flag
+                    decimal = false;
+                    //display expression
+                    //check for division by zero error
+                    if((valuesEntered[1] === '/') && (valuesEntered[2] === 0 )){
+                        setContent('Division by zero error!');
+                        valuesEntered[2] = null;
+                    }else{
+                        validateInput(parseFloat(keysEntered));
+                        obtainResult();
+                        //cancel previous
+                        lastValueWasOperator = false;
+                    }
+                }
             }
+
             break;
         }
 
@@ -426,7 +375,6 @@ function modifyInput(id){
             content.textContent='';
             keysEntered = '';
             valuesEntered = [];
-            //console.log(`342- ${valuesEntered[0]}`);
             numLength = 0;
             decimal = false;
             lastValueWasOperator=false;
@@ -435,109 +383,63 @@ function modifyInput(id){
             operator = null;
             result = 0;
             clearHistoryDisplay();
-           // historicEntries.length = 0;
+            historicEntries.length = 0;
             break;
         }
         case 'del': {
-           // console.log('del');
-            if(lastValueWasOperator){
-                lastValueWasOperator = false;
-                valuesEntered[1]='';
-            }else{
-                revStr = keysEntered.slice(0,keysEntered.length-1);
-                console.log(`revStr = ${revStr}`);
+                console.log(`keysEntered = ${keysEntered}`);
+                let cutStr = keysEntered.slice(0,1); // first char
+                revStr = keysEntered.slice(0,keysEntered.length-1); // the rest
+                console.log(`cutStr = ${cutStr} , revStr = ${revStr}`);
                 //check if just deleted decimal point and reset
                 if( keysEntered.includes('.') && (!(revStr.includes('.'))) ){decimal = false;}
+                //check if just deleted operator and reset
+                if(( cutStr.includes('+' || '-') || '/' || '*')
+                        && (operator === cutStr )) {
+                            lastValueWasOperator = false;
+                            valuesEntered[1]='';
+                    }
                 keysEntered = '';
-                //console.log('@.. ' + keysEntered);
                 keysEntered = revStr;
-                //content.textContent= '';
-                //setTimeout(()=>{}, 5000);
-                console.log('keysEntered now = ' + keysEntered);
-                //content.textContent= keysEntered;
                 setContent(keysEntered);
-            }
+            
             break;
         }
         case 'sign':{
             //are we changing first or second value
             console.log(`445 - first is ${first} second is ${second} !`);
-            //console.log(second.toString());
             let currentSign = keysEntered.slice(0,1);
             console.log('448' + keysEntered +', '+ currentSign);
             if(currentSign === '-'){
                 revStr=keysEntered.replace('-','+');
                 keysEntered ='';
                 keysEntered= revStr;
-                console.log('453- '+keysEntered);
             }else{
                 keysEntered = '-'+ keysEntered;
-                console.log('456- ' + keysEntered);
                 content.textContent = keysEntered;
             }
             if(first === null){
-                console.log('460- first = null');
-                //content.textContent = keysEntered;
                 setContent(keysEntered);
             }
-            //reusing calculated value as first
-            //console.log(`first is ${first} operator is ${operator}`);
-            
-            if(!(first === null)){console.log(`467- first has value ${first}`);}
-            else{/*
-                console.log('469-'+ keysEntered);
-                console.log('470-'+ keysEntered);
-                if(keysEntered.slice(0,1 === '-')){
-                    revStr = '+'+keysEntered.slice(1, keysEntered.length);  //- -> +
-                }else{
-                    keysEntered = '-'+keysEntered;  //+ -> -
-                }
-                //content.textContent = valuesEntered[0].toString(); 
-                setContent(keysEntered);
-                console.log('478-' + keysEntered);
-                */
-            }
-            if(operator === null){console.log(`468- operator ${operator} is null`);}
-            else{
-                console.log(`470- operator is ${operator} `);
-            }
-            
             if((!(first === null))&&(operator === null)){
-                console.log('474-'+ valuesEntered[0]);
                 valuesEntered[0]= valuesEntered[0]*-1;  // + -> - or - -> +
-                //content.textContent = valuesEntered[0].toString(); 
                 setContent(valuesEntered[0].toString());
-                console.log('478-' + valuesEntered[0]);
             }else{
-                console.log(`480- valuesEntered[0] is ${valuesEntered[0]}`);
-                console.log(`481- valuesEntered[1] is ${valuesEntered[1]}`);
                 if(!(valuesEntered[0] === (null || undefined) ) && !(valuesEntered[1]=== (null || undefined))){
                     const temp0 = valuesEntered[0].toString();
                     const temp1 = valuesEntered[1].toString();
-                    //content.textContent = `${temp0} ${temp1} ${keysEntered}`;
                     setContent(`${temp0} ${temp1} ${keysEntered}`);
-                }else{
-                    //content.textContent = `error at 488`;
-                   // setContent(`error at 489`);
                 }
             }
-            
             break;
         }
     }
 }
 
 function obtainResult() {
-    //console.log(`381-result : ${result}`);
-    //console.log(`operate( ${valuesEntered[0]}, ${valuesEntered[1]}, ${valuesEntered[2]})`);
-    //console.log(typeof valuesEntered[0]);
-   // console.log(typeof valuesEntered[1]);
-    //console.log(typeof valuesEntered[2]);
     result =  operate(valuesEntered[0], valuesEntered[1], valuesEntered[2]);
-    console.log(`444- result = ${result}` );
     //avoid rounding errors to many dec places
     result = parseFloat(result.toFixed(6)); //6 dec places
-    console.log(`447- result = ${result}` );
     const resultObj = {
         'result' : result,
         'first' : valuesEntered[0],
@@ -545,11 +447,6 @@ function obtainResult() {
         'second': valuesEntered[2]
     }
     historicEntries.push(resultObj);
-    console.log(`503-result : ${result}`);
-    console.log(`504-from: ${resultObj.first} ${resultObj.operator} ${resultObj.second}`);
-    for(let i=0; i< historicEntries.length; i++){
-        console.log(`506-historic: ${historicEntries[i].first} ${historicEntries[i].operator} ${historicEntries[i].second} = ${historicEntries[i].result}`);
-    }
     content.textContent=result;
     setContent(result.toString());
     showHistory();
@@ -557,7 +454,6 @@ function obtainResult() {
 }
 
 function resetValues(){
-    console.log('resetting values');
     keysEntered = '';
     valuesEntered = [];
     numLength = 0;
@@ -571,14 +467,12 @@ function resetValues(){
 }
 
 function setContent(text){
-    //console.log('at setContent(text)');
     if(text.length > 16){
         content.style.fontSize= '20px';
     }else{
         content.style.fontSize= '40px';
     }
     content.textContent = text;
-    //console.log('post setContent(text)');
 }
 
 
@@ -591,9 +485,6 @@ buttons.forEach(function(button){
 });
 
 
-
 //run code
 noflow();
 
-
-//test code
